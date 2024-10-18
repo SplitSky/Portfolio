cba9a57864bacbd4f6907feb54fe972a5ca8087b
SplitSky <opop.neska60@gmail.com>
2024-10-18 18:19:36 +0100

    Initialise the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Updated to support TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
