2c7bd12e8997a3274105b0b16a64148bfede077d
SplitSky <opop.neska60@gmail.com>
2024-10-20 21:56:01 +0100

    Changes to config. Compiling is not working due to jsx rendering bug. It's a limitation of react within vite. Workaround is explicit declaration of JSX:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection.tsx';
-import ProjectPreview from '../components/ProjectPreview.tsx'
 
 const Home: React.FC = () => {
   return (
